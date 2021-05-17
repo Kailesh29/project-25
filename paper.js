7b8c2b618a9068cd268bfe55e4061d1bc2acf0e0
Kailesh29 <82511713+Kailesh29@users.noreply.github.com>
2021-05-17 11:50:56 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,27 @@
+class paper {
+    constructor(x,y,radius) {
+var options={
+    isStatic : false,
+    restitution : 0.3,
+    friction : 0,
+    density : 1.2,
+}
+    
+this.radius = radius;
+this.body = Bodies.circle(x, y, this.radius, options);
+
+this.image = loadImage("paper.png");
+World.add(world, this.body);
+}
+display(){
+
+var pos = this.body.position;
+push();
+translate(pos.x, pos.y);
+imageMode(CENTER);
+image(this.image, 0, 0, this.radius,this.radius);
+pop();
+
+}
+}
+
